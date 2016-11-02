b459ae938635335d924eb419a6037dac4db029d1
User <User@BigoS>
2016-11-02 20:22:49 +0100

    lesson 2 lesson tasks

--- Lesson2/Home/src/Main.java
+++ Lesson2/Home/src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+ 
+public class Main{
+  public static void main(String[] args){
+      String imie;
+      Scanner odczyt = new Scanner(System.in);
+ 
+      imie = odczyt.nextLine();
+ 
+      System.out.println("Witaj "+imie);
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World Develop");
-	}
-
+  }
 }
