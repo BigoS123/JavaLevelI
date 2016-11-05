7209a0ce31eb60a509beef45ccb7ce53f581f5e3
User <User@BigoS>
2016-11-05 14:02:49 +0100

    Zadanie domowe i to z lekcjii cz.1

---
+++ Lesson2/HomeTask2/src/Main.java
@@ -0,0 +1,43 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		char initialOfName;
+		String surname;
+		Short temperature;
+		Float preciseTemperature;
+		short studentsInClass;
+		Float bodyWeight;
+		Float bodyHeight;
+		int mountEverestHeight;
+		boolean isMailboxEmpty;
+		long neuronsInBrain;
+		double piNumber;
+		int facebookUsers;
+		
+		initialOfName = 'M';
+		surname = "£anohca";
+		temperature = 24;
+		preciseTemperature = (float) 13.5;
+		studentsInClass = 32;
+		bodyWeight = (float) 55.4;
+		bodyHeight = (float) 162.5;
+		mountEverestHeight = 884800;
+		isMailboxEmpty = true;
+		
+		System.out.println("Inicja³ imienia: " + initialOfName);
+		System.out.println("Nazwisko: " + surname);
+		System.out.println("Temperatura (w stopniach celcjusza): " + temperature);
+		System.out.println("Dok³adna temperatura (w stopniach celcjusza): " + preciseTemperature);
+		System.out.println("Liczba uczniów w klasie: " + studentsInClass);
+		System.out.println("Masa cia³a (w kg): " + bodyWeight);
+		System.out.println("Wzrost (w cm): " + bodyHeight);
+		System.out.println("Wysokoœæ Mount Everestu (w cm): " + mountEverestHeight);  
+		System.out.println("Czy skrzynka pocztowa jest pusta: " + isMailboxEmpty);
+		
+		
+
+	}
+
+}
