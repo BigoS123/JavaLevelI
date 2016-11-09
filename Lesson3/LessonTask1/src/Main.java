4e1d1307cd363422d3304c4d035741cf72ea62ea
User <User@BigoS>
2016-11-09 19:50:34 +0100

    lesson 3

---
+++ Lesson3/LessonTask1/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("KALKULATOR");
+	
+		System.out.println();
+		
+		double ammount1 = 0;
+		double ammount2 = 0;
+		int operation = 0;
+		
+		
+		System.out.println("Wybierz numer operacji z menu: ");
+		switch(operation)
+		{
+			case 1:
+				System.out.println("Dodawanie");
+				System.out.println(ammount1 +  " + " + ammount2 + " = " + (ammount1 + ammount2));
+				break;
+		}
+
+	}
+
+}
