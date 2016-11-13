349635b37d1a24d77bc0968250996c36cf90ec57
User <User@BigoS>
2016-11-13 15:12:49 +0100

    zadania cz2

--- Lesson3/HomeTask3/src/Main.java
+++ Lesson3/HomeTask3/src/Main.java
@@ -1,9 +1,115 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Moj odtwarzacz muzyczny");
+		System.out.println();
+		
+		int typeOfMusic;
+		int volume;
+		int tone1;
+		int tone2;
+		int tone3;
+		int rightSpeaker;
+		
+		System.out.println("Wybierz swoj ulubiony gatunek muzyczny: 1- rock 2- jazz 3- pop 4- rnb");
+		Scanner odczyt1 = new Scanner(System.in);
+		typeOfMusic = odczyt1.nextInt();
+		System.out.println("Wybierz glosnosc muzyki: 1- glosno 2-cicho ");
+		Scanner odczyt2 = new Scanner(System.in);
+		volume = odczyt2.nextInt();
+		System.out.println("Wybierz ton w jakim lubisz sluchac muzyki 1-niskie 2-srednie 3- wyskoie");
+		Scanner odczyt3 = new Scanner(System.in);
+		tone1 = odczyt3.nextInt();
+		System.out.println("Wybierz ton w jakim lubisz sluchac muzyki 0- jesli lubisz tylko jeden 1-niskie 2-srednie 3- wyskoie ");
+		Scanner odczyt4 = new Scanner(System.in);
+		tone2 = odczyt4.nextInt();
+		System.out.println("Wybierz ton w jakim lubisz sluchac muzyki 0- jesli lubisz tylko jeden lub dwa  1-niskie 2-srednie 3- wyskoie ");
+		Scanner odczyt5 = new Scanner(System.in);
+		tone3 = odczyt5.nextInt();
+		System.out.println("Wybierz zakres DB dla prwaego glosnika (max 10 DB, lewy ustwia sie automatycznie)");
+		Scanner odczyt6 = new Scanner(System.in);
+		rightSpeaker = odczyt6.nextInt();
+		
+		System.out.println();
+		
+		switch(typeOfMusic)
+		{
+			case 1:
+				System.out.println("Twoj ulubiony gatunek muzyczbny to: rock");
+				break;
+			case 2:
+				System.out.println("Twoj ulubiony gatunek muzyczbny to: jazz");
+				break;
+			case 3:
+				System.out.println("Twoj ulubiony gatunek muzyczbny to: pop");
+				break;
+			case 4:
+				System.out.println("Twoj ulubiony gatunek muzyczbny to: rnb");
+				break;
+		
+		}
+		
+		System.out.println();
+		System.out.println("Wybrana glosnosc musyki: ");
+		System.out.println(volume == 1 ? "glosno" : "cicho");
+		System.out.println();
+		
+		switch(tone1)
+		{
+			case 0:
+			System.out.println();
+				break;
+			case 1:
+				System.out.println("Twoje ulubione tony: niskie");
+				break;
+			case 2:
+				System.out.println("Twoje ulubione tony: srednie");
+				break;
+			case 3:
+				System.out.println("Twoje ulubion tony: wysokie");
+				break;
+		
+		}
+		switch(tone2)
+		{
+		case 0:
+			System.out.println();
+			break;
+		case 1:
+			System.out.println("Twoje ulubione tony: niskie");
+			break;
+		case 2:
+			System.out.println("Twoje ulubione tony: srednie");
+			break;
+		case 3:
+			System.out.println("Twoje ulubion tony: wysokie");
+			break;
+			
+		}
+		switch(tone3)
+		{
+		case 0:
+			System.out.println();
+			break;
+		case 1:
+			System.out.println("Twoje ulubione tony: niskie");
+			break;
+		case 2:
+			System.out.println("Twoje ulubione tony: srednie");
+			break;
+		case 3:
+			System.out.println("Twoje ulubion tony: wysokie");
+			break;
+			
+		}
+	
+		
+		System.out.println("balans pomiedzy glosnikami: " + rightSpeaker + " prawy " + (10-rightSpeaker) + "lewy");
+		
+		
 	}
 
 }
--- Lesson3/HomeTask4/src/Main.java
+++ Lesson3/HomeTask4/src/Main.java
@@ -1,9 +1,67 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("KALKULATOR PÓL");
+		System.out.println();
+		
+		double sideA;
+		double sideB;
+		double radius;
+		double height;
+		double base;
+		int task;	
+		
+		System.out.println("MENU");
+		System.out.println("1- kwadrat");
+		System.out.println("2- prostoka");
+		System.out.println("3- kolo");
+		System.out.println("4- trojkat");
+		System.out.println();
+		System.out.println("Wybierz figure by obliczyc jej pole");
+		
+		
+		Scanner odczyt1 = new Scanner(System.in);
+		task = odczyt1.nextInt();
+		
+		switch(task)
+		{
+			case 1:
+				System.out.println("Wprowadz bok kwadratu:");
+				Scanner odczyt2 = new Scanner(System.in);
+				sideA = odczyt2.nextInt();
+				System.out.println("Pole kwadratu wynosi: " + sideA + " * " + sideA  + " = " + (sideA * sideA));
+				break;
+			case 2:
+				System.out.println("Wprowadz krotszy bok prostokata: ");
+				Scanner odczyt3 = new Scanner(System.in);
+				sideA = odczyt3.nextInt();
+				System.out.println("Wprowadz dluzszy bok prostokata: ");
+				Scanner odczyt4 = new Scanner(System.in);
+				sideB = odczyt4.nextInt();
+				System.out.println("Pole prostokata wynosi: " + sideA + " * " + sideB  + " = " + (sideA * sideB));
+				break;
+			case 3:	
+				System.out.println("Wprowadz promien kola:");
+				Scanner odczyt5 = new Scanner(System.in);
+				radius = odczyt5.nextInt();
+				System.out.println("Pole kola wynosi: " + "3.14" + " * " + radius + " * " + radius + " = " + (3.14*radius*radius));
+				break;
+			case 4:
+				System.out.println("Wprowadz wysokosc trojkata:");
+				Scanner odczyt6 = new Scanner(System.in);
+				height = odczyt6.nextInt();
+				System.out.println("Wprowadz podstawe trojkata:");
+				Scanner odczyt7 = new Scanner(System.in);
+				base = odczyt7.nextInt();
+				System.out.println("Pole trojkata wynosi: " + height + " * " + base + " = " + (height*base));
+				break;
+		}
+		
+		
+		
 	}
 
 }
