92e80cc395c73b1010b4ddfbf6d06c1b8f31563b
User <User@BigoS>
2016-11-23 19:28:35 +0100

    poprawa zad 3 z lecji 3

--- Lesson3/HomeTask3/src/Main.java
+++ Lesson3/HomeTask3/src/Main.java
@@ -9,7 +9,9 @@ public class Main {
 
 		int typeOfMusic;
 		String volume;
-		String tone;
+		boolean toneLow;
+		boolean toneMedium;
+		boolean toneHigh;
 		int rightSpeaker;
 
 		System.out.println("Wybierz swoj ulubiony gatunek muzyczny: 1- rock 2- jazz 3- pop 4- rnb");
@@ -18,9 +20,18 @@ public class Main {
 		System.out.println("Wprowadz glosnosc muzyki (Glosno, Cicho) ");
 		Scanner scanner1 = new Scanner(System.in);
 		volume = scanner1.nextLine();
-		System.out.println("Wprowadz tony w jakich lubisz sluchac muzyki (niskie srednie wyskoie)");
+
+		System.out.println("Czy lubisz sluchac w tonach niskich?(true false)");
 		Scanner scanner2 = new Scanner(System.in);
-		tone = scanner2.nextLine();
+		toneLow = scanner2.nextBoolean();
+		System.out.println("Czy lubisz sluchac w tonach srednich?(true false)");
+		toneMedium = scanner2.nextBoolean();
+		if (!toneLow && !toneMedium) {
+			toneHigh = true;
+		} else {
+			System.out.println("Czy lubisz sluchac w tonach wysokich?(true false)");
+			toneHigh = scanner2.nextBoolean();
+		}
 		System.out.println("Wybierz zakres DB dla prwaego glosnika (max 10 DB, lewy ustwia sie automatycznie)");
 		rightSpeaker = scanner.nextInt();
 
@@ -45,8 +56,17 @@ public class Main {
 		System.out.println();
 		System.out.println("Wybrana glosnosc musyki: " + volume);
 
-		System.out.println("Wybrane tony muzyki: " + tone);
-		
+		System.out.println("Wybrane tony muzyki: ");
+		if (toneLow) {
+			System.out.println("Tony niskie ");
+		}
+		if (toneMedium) {
+			System.out.println("Tony srednie ");
+		}
+		if (toneHigh) {
+			System.out.println("Tony wysokie ");
+		}
+
 		System.out.println("balans pomiedzy glosnikami: " + rightSpeaker + " prawy " + (10 - rightSpeaker) + " lewy");
 
 	}
