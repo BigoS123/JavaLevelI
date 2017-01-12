eca24e307e7677e722a603620d9be5d8ac77567a
User <User@BigoS>
2017-01-12 21:52:55 +0100

    zadanie robione na lekcji, wymyslilem projekt zrobienia gry z motywem zbierania punktow i przechodzenia poziomow, zrobilem pierwszy poziom :D

--- Lesson6/HomeTask1/src/Main.java
+++ Lesson6/HomeTask1/src/Main.java
@@ -1,59 +1,114 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+	static ArrayList<String> shoppingList = new ArrayList<String>();
+	static ArrayList<Integer> shoppingListPrices = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
+		validateInputInt("podaj wartosc ");
+		System.out.println("przeszlo");
+		// showOperations();
+	}
 
-		ArrayList<String> shoppingList = new ArrayList<String>();
+	public static void showOperations() {
 
-		shoppingList.add("Mleko");
-		shoppingList.add("Ser");
-		shoppingList.add("Szynka");
-		shoppingList.add("Maslo");
-		shoppingList.add("Chleb");
-		shoppingList.add("Jogurty");
-		shoppingList.add("Pasztet");
-		shoppingList.add("Majonez");
+		for (;;) {
 
-		for (int i = 0; i < shoppingList.size(); i++) {
-			System.out.println(shoppingList.get(i));
-		}
+			Scanner sc = new Scanner(System.in);
 
-		shoppingList.remove(shoppingList.get(0));
-		shoppingList.remove(shoppingList.get(3));
-		shoppingList.remove(shoppingList.get(4));
-		System.out.println();
-		for (int i = 0; i < shoppingList.size(); i++) {
-			System.out.println(shoppingList.get(i));
+			System.out.println("Menu");
+			System.out.println("1- dodaj produkt do listy zakupow");
+			System.out.println("2- usun produkt z listy zakupow");
+			System.out.println("3- wyswietl liste zakupow");
+			System.out.println("4- zakoncz zakupy");
+			System.out.println();
 
-		}
-
-		shoppingList.add("Bulki");
-		shoppingList.add("Nutella");
-		shoppingList.add("Jajka");
-		shoppingList.add("Piwo");
-		shoppingList.add("Oliwki");
-		shoppingList.add("Chipsy");
-
-		Random generator = new Random();
-		ArrayList<Integer> shoppingListPrices = new ArrayList<Integer>();
-
-		if (shoppingList.size() > 10) {
-			for (int i = 0; i < shoppingList.size(); i++) {
-				shoppingListPrices.add(generator.nextInt((24) + 1));
+			int menuPosition = sc.nextInt();
+			switch (menuPosition) {
+			case 1:
+				addProduct();
+				break;
+			case 2:
+				Scanner sc1 = new Scanner(System.in);
+				System.out.println("Wskaz pozycje z listy do usuniecia");
+				int position = sc1.nextInt();
+				deleteProduct(position);
+				break;
+			case 3:
+				clearingConsole();
+				showListOfProducts();
+				break;
+			case 4:
+				showTotalPrice();
+				break;
+			default:
+				System.out.println("Brak danej pozycji w menu");
+				break;
 			}
+			clearingConsole();
 		}
-		System.out.println();
+	}
+
+	public static void validateInputInt(String comunicate) {
+		Scanner sc = new Scanner(System.in);
+		int number;
+		do {
+			System.out.println(comunicate);
+			while (!sc.hasNextInt()) {
+				String input = sc.next();
+				System.out.printf("%s nie jest poprawnym numerem \n", input);
+			}
+			number = sc.nextInt();
+		} while (number < 0);
+
+	}
+
+	public static int generatePrice() {
+		Random generator = new Random();
+
+		return generator.nextInt((25) + 1);
+
+	}
+
+	public static void addProduct() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Wprowadz nazwe produktu");
+		String product = sc.nextLine();
+		shoppingList.add(product);
+		shoppingListPrices.add(generatePrice());
+	}
+
+	public static void showListOfProducts() {
 		for (int i = 0; i < shoppingList.size(); i++) {
-			System.out.println("Cena za " + shoppingList.get(i) + " wynosi " + shoppingListPrices.get(i));
+			System.out
+					.println("Produkt nr " + (i + 1) + " " + shoppingList.get(i) + " za " + shoppingListPrices.get(i));
 		}
+	}
+
+	public static void clearingConsole() {
+		for (int i = 0; i < 20; i++) {
+			System.out.println();
+		}
+	}
+
+	public static void deleteProduct(int productsNumber) {
+		shoppingList.remove((productsNumber - 1));
+		shoppingListPrices.remove((productsNumber - 1));
+	}
+
+	public static int sumUpPrice() {
 		int totalPrcie = 0;
-		for (int i = 0; i < shoppingList.size(); i++) {
+		for (int i = 0; i < shoppingListPrices.size(); i++) {
 
 			totalPrcie = totalPrcie + shoppingListPrices.get(i);
 		}
-		System.out.println();
-		System.out.println("Zakupy beda kosztowac " + totalPrcie);
+		return totalPrcie;
+	}
+
+	public static void showTotalPrice() {
+		System.out.println("Laczna suma zakupow wynosi " + sumUpPrice());
 	}
 }
