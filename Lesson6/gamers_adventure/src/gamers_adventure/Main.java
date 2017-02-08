9f0f595d2f52d37ff7d92822253391b8eeee621d
User <User@BigoS>
2017-02-08 20:46:07 +0100

    zrobione zadanie bez refactoringu, wrzuce w weekend sktocona wersje

--- Lesson6/gamers_adventure/src/gamers_adventure/Main.java
+++ Lesson6/gamers_adventure/src/gamers_adventure/Main.java
@@ -4,27 +4,20 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
-//TODO 1) Usunac niepotrzebny kod
-//TODO 2) 
-//TODO 3)generowanie mapy (poprawic i przetestowac/przedebugowac- nie dziala jeszcze zawsze)
-//TODO 4)generowanie klucza do mapy
-//TODO 5)lista zawierajaca mapy z poziomami
+//TODO 1) Refactofing kodu, uporz¹dkowac kod i popatrzeæ gdzie jeszcze mo¿na by stworzyæ funkcjê
+
 
 public class Main {
 	static int compuerLevel = 1;
 	static int currency;
-	static boolean isLevelPassed;
 	static char[][] gameBoard = new char[15][20];
 	static final int NUMBERS_OF_AVAILABLE_FIELDS = 30;
-	static final String[] DIRECTIONS = { "right", "left", "up", "down" };
-
+	static final String[] DIRECTIONS = { "right", "up", "down" };
+	static char[] gameKey = new char[NUMBERS_OF_AVAILABLE_FIELDS];
 	static HashMap<String, Boolean> availableDirection = new HashMap<String, Boolean>();
 
 	public static void main(String[] args) {
-		availableDirectionMap();
-		generateEmptyBoard();
-		generateMap();
-		showBoard();
+		mainMenu();
 	}
 
 	public static void availableDirectionMap() {
@@ -37,6 +30,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int choice;
 		for (;;) {
+			clearConsole();
 			System.out.println("Wybierz dzia³anie");
 			System.out.println("0- Rozegraj grê");
 			System.out.println("1- Zobacz ilosc sowich monet");
@@ -50,18 +44,23 @@ public class Main {
 				oneRoundOfGame();
 				break;
 			case 1:
+
 				System.out.println("Twoja ilosc monet to " + currency);
 				break;
 			case 2:
-
+	
+				pricesOfUpgrades();
 				break;
 			case 3:
 
+				System.out.println("Pozion twojego komputera " + compuerLevel);
 				break;
 			case 4:
-
+			
+				upgradesSystem();
 				break;
 			default:
+			
 				System.out.println("Brak danej pozycji w menu");
 				break;
 			}
@@ -79,57 +78,55 @@ public class Main {
 
 	}
 
-	public static void upgradesSystem(int usersCurrency) {
+	public static void upgradesSystem() {
 		Scanner sc = new Scanner(System.in);
 		int choice;
 		System.out.println("Witaj w sklepie");
 		System.out.println("Wybier poziom na ktory chcesz ulepszyc komputer");
 		choice = sc.nextInt();
 		switch (choice) {
-		case 1:
-
+		case 2:
+			if (currency >= 5) {
+				currency = currency - 5;
+			} else {
+				System.out.println("Masz za malo monet");
+			}
 			break;
+		case 3:
+			if (currency >= 10) {
+				currency = currency - 10;
+			} else {
+				System.out.println("Masz za malo monet");
+			}
+			break;
+		case 4:
+			if (currency >= 20) {
+				currency = currency - 20;
+			} else {
+				System.out.println("Masz za malo monet");
+			}
+			break;
+		default:
+			System.out.println("Nie ma takiego pziomu komputera");
+			
 		}
 
 	}
 
 	public static void oneRoundOfGame() {
-		chosingLevel();
-		levelsReward();
+		availableDirectionMap();
+		generateEmptyBoard();
+		generateMap();
+		showBoard();
+		levelsReward(compareKey(getUsersKey(), String.valueOf(gameKey)));
 	}
 
-	public static void levelsReward() {
-		if (isLevelPassed) {
+	public static void levelsReward(boolean passedLevel) {
+		if (passedLevel) {
 			int currencyGained = compuerLevel * 1;
 			currency = currency + currencyGained;
 			System.out.println("Masz obecnie: " + currency + " kredytow");
 		}
-	}
-
-	public static boolean chosingLevel() {
-		Random generator = new Random();
-		int levelNumber = 1;
-		// int levelNumber = generator.nextInt(4);
-		switch (levelNumber) {
-		case 0:
-			level1();
-			break;
-		case 1:
-			level2();
-			break;
-		// case 2:
-		//
-		// break;
-		// case 3:
-		//
-
-		//
-		// break;
-		default:
-			break;
-		}
-		return isLevelPassed;
-
 	}
 
 	public static void generateEmptyBoard() {
@@ -159,18 +156,19 @@ public class Main {
 
 		for (int i = 0; i < NUMBERS_OF_AVAILABLE_FIELDS; i++) {
 			checkPossibilities(positionX, positionY);
-			if (DIRECTIONS[chosePosibility()] == DIRECTIONS[0]) {
+			int indexDirection = chosePosibility();
+			if (DIRECTIONS[indexDirection] == DIRECTIONS[0]) {
 				positionY = positionY + 1;
 				gameBoard[positionX][positionY] = 'x';
-			} else if (DIRECTIONS[chosePosibility()] == DIRECTIONS[1]) {
-				positionY = positionY - 1;
-				gameBoard[positionX][positionY] = 'x';
-			} else if (DIRECTIONS[chosePosibility()] == DIRECTIONS[2]) {
-				positionX = positionX + 1;
-				gameBoard[positionX][positionY] = 'x';
-			} else {
+				gameKey[i] = 'd';
+			} else if (DIRECTIONS[indexDirection] == DIRECTIONS[1]) {
 				positionX = positionX - 1;
 				gameBoard[positionX][positionY] = 'x';
+				gameKey[i] = 'w';
+			} else {
+				positionX = positionX + 1;
+				gameBoard[positionX][positionY] = 'x';
+				gameKey[i] = 's';
 			}
 		}
 
@@ -182,22 +180,18 @@ public class Main {
 		} else {
 			availableDirection.replace(DIRECTIONS[0], false);
 		}
-		if ((y - 1) > 0 && gameBoard[x][y - 1] == '@') {
+
+		if ((x - 1) > 0 && (y - 1) >= 0 && gameBoard[x - 1][y] == '@' && gameBoard[x - 1][y - 1] == '@') {
 			availableDirection.replace(DIRECTIONS[1], true);
 		} else {
 			availableDirection.replace(DIRECTIONS[1], false);
 
 		}
-		if ((x + 1) < gameBoard.length && gameBoard[x + 1][y] == '@') {
+		if ((x + 1) < gameBoard.length && (y - 1) >= 0 && gameBoard[x + 1][y] == '@'
+				&& gameBoard[x + 1][y - 1] == '@') {
 			availableDirection.replace(DIRECTIONS[2], true);
 		} else {
 			availableDirection.replace(DIRECTIONS[2], false);
-
-		}
-		if ((x - 1) > 0 && gameBoard[x - 1][y] == '@') {
-			availableDirection.replace(DIRECTIONS[3], true);
-		} else {
-			availableDirection.replace(DIRECTIONS[3], false);
 
 		}
 
@@ -213,83 +207,31 @@ public class Main {
 		return option;
 	}
 
-	public static void level1() {
-		Scanner sc = new Scanner(System.in);
-		String key = "wwdwwawwwwdww";
-		System.out.println("           ___ __  						  ");
-		System.out.println("          |  _ __ x|   				   	  ");
-		System.out.println("          | |							  ");
-		System.out.println("          | |							  ");
-		System.out.println("    ___ __| |	     					  ");
-		System.out.println("   |  _ __ _|                       	  ");
-		System.out.println("   | |									  ");
-		System.out.println("   |o|		                 			  ");
-		System.out.println("o- twoja pozycja, dojdz do x wpisujac droge");
-		System.out.println("W- kork w gore|A-obrot w lewo|D-obrot w pawo");
-		System.out.println();
-		System.out.println("Wprowadz rozwiazanie");
-		String answer = sc.nextLine();
-		switch (answer) {
-		case "wwdwwawwwwdww":
-			isLevelPassed = true;
-			for (int i = 0; i < 10; i++) {
-				System.out.println();
-			}
-			System.out.println("           ___ __  						  ");
-			System.out.println("          |  _ __ o|   				   	  ");
-			System.out.println("          | |							  ");
-			System.out.println("          | |							  ");
-			System.out.println("    ___ __| |	     					  ");
-			System.out.println("   |  _ __ _|                       	  ");
-			System.out.println("   | |									  ");
-			System.out.println("   | |		                 			  ");
-			System.out.println();
-			System.out.println("Gratulacje udalo ci sie przejsc poziom");
-			break;
+	public static boolean compareKey(String answer, String labirynthKey) {
 
-		default:
-			isLevelPassed = false;
-			System.out.println("Niestety nie przeszedles poziomu");
-			break;
+		if (answer.equals(labirynthKey)) {
+			System.out.println("Gratulacje przeszedles labirynt");
+			return true;
+		} else {
+			System.out.println("Nie udalo ci sie przejsc labiryntu wprowadz klucz ponownie");
+			return false;
 		}
 
 	}
 
-	public static void level2() {
-		Scanner sc = new Scanner(System.in);
-		String key = "wwwdwwdwwwawwawwww";
-		System.out.println("                  ___                      ");
-		System.out.println("     ___ __ _     |x|                      ");
-		System.out.println("    |  _ __  |    | |                      ");
-		System.out.println("    | |    | |    | |                      ");
-		System.out.println("    | |    | |_ __| |                      ");
-		System.out.println("    |o|    |___ __ _|                      ");
-		System.out.println("                                           ");
-		System.out.println("o- twoja pozycja, dojdz do x wpisujac droge");
-		System.out.println("W- kork w gore|A-obrot w lewo|D-obrot w pawo");
+	public static void showKey() {
+		System.out.print(gameKey);
+	}
 
-		String answer = sc.nextLine();
-		switch (answer) {
-		case "wwwdwwdwwwawwawwww":
-			isLevelPassed = true;
-			for (int i = 0; i < 10; i++) {
-				System.out.println();
-			}
-			System.out.println("                  ___                      ");
-			System.out.println("     ___ __ _     |o|                      ");
-			System.out.println("    |  _ __  |    | |                      ");
-			System.out.println("    | |    | |    | |                      ");
-			System.out.println("    | |    | |_ __| |                      ");
-			System.out.println("    | |    |___ __ _|                      ");
-			System.out.println("                                           ");
+	public static void clearConsole() {
+		for (int i = 0; i < 10; i++) {
 			System.out.println();
-			System.out.println("Gratulacje udalo ci sie przejsc poziom");
-			break;
-		default:
-			isLevelPassed = false;
-			System.out.println("Niestety nie przeszedles poziomu");
-			break;
-
 		}
+	}
+
+	public static String getUsersKey() {
+		System.out.println("Wprowadz klucz do labiryntu");
+		Scanner sc = new Scanner(System.in);
+		return sc.nextLine();
 	}
 }
